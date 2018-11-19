e0946a3caa67910110c0cb6bc3edf905d7b0cfca
rish <coderfox98@gmail.com>
2018-11-19 23:39:57 +0530

    Initial Commit

---
+++ Motivational Quotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Motivational Quotes
+//
+//  Created by Rish on 19/11/18.
+//  Copyright © 2018 Rish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
